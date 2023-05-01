1e69985e60009adc37ca777fc6972c07ffbd637c
DenisFive5 <123938443+DenisFive5@users.noreply.github.com>
2023-05-01 15:33:24 +0200

    Add files via upload

---
+++ Nous.php
@@ -0,0 +1,62 @@
+<!doctype html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Développement professionnel</title>
+		<link href="Styles.css" rel="stylesheet" type="text/css">
+  </head>
+  <header>
+	<nav>
+		<label for="menu-mobile" class="menu-mobile"><b>Menu</b></label>
+		<input type="checkbox" id="menu-mobile" role="button">
+		   <ul>
+			   <li class="menu-html"><a href= "Index.html"><b><i>Accueil</i></b></a>
+				   <ul class="submenu">
+					  <li><a href="HTML.html">Mon portofolio</a></li>
+					  <li><a href="Monstage.html">Mon stage en entreprise</a></li>
+					  <li><a href="Maformation.html">Ma formation</a></li>
+					  <li><a href="#">Mon expérience</a></li>
+				   </ul>
+			   </li>			   
+			   <li class="menu-css"><a href= "CSS.html"><b><i>À propos</i></b></a>
+					<ul class="submenu">
+					  <li><a href="#">Mon "Creative Cloud"</a></li>
+					  <li><a href="#">Mes projets</a></li>
+					  <li><a href="#">Mon web design</a></li>
+					</ul>
+			   </li> 
+			   <li class="menu-Javascript"><a href= "Javascript.html"><b><i>Galerie</i></b></a>
+				  <ul class="submenu">
+					  <li><a href="#">Mon book</a></li>
+					  <li><a href="#">Photoshop</a></li>						  
+				  </ul>
+			   </li>
+			  <li class="menu-Contact"><a href= "Contactezmoi !.html"><b><i>Contact</i></b></a>
+				  <ul class="submenu">
+				     <li><a href="Coordonnees.html">Vos coordonnées</a></li>						  						  
+				  </ul>
+			   </li> 
+			   <li class="menu-Nous"><a href= "Contactezmoi !.html"><b><i>Développement professionnel</i></b></a>
+				<ul class="submenu">
+				   <li><a href="Veilletechnologique.html">Veille technologique</a></li>						  						  
+			  </ul>
+		  </ul>			 
+	 </nav>	  
+  </header>
+  <body>
+	  <br>
+	  <br>
+	  <br>
+	  <br>
+	  <h1><i><u><b>Vos infos</b></u></i></h1>
+	  <br>
+	  <br>
+   <img src="Images/PotoFolio.gif" width="500" height="500" alt=""/>
+	  <center>  
+	    <form  method="POST" action="" enctype="multipart/form-data">
+	          <input type="file" name="file"><br>
+			</br><input type="submit" name="valider" value="Envoyer">
+	    </form>
+	</center> 
+  </body>
+</html>
